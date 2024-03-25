d0096776c34c92bcc23977bd684584abbe6867bf
raphael assor raphaelassor@rabcommerce.com <raphaelassor@rabcommerce.com>
2024-03-25 11:23:27 +0200

    fixed bug and organized files

---
+++ utils/params.py
@@ -0,0 +1,24 @@
+animals = [
+    {"name": "Cat", "image": "animal_images/cat.jpg", "description": "Cat"},
+    {"name": "Dog", "image": "animal_images/dog.jpg", "description": "Dog"},
+    {"name": "Bird", "image": "animal_images/bird.jpg", "description": "Bird"},
+    {"name": "Rabbit", "image": "animal_images/rabbit.jpg", "description": "Rabbit"},
+    {"name": "Hamster", "image": "animal_images/hamster.jpg", "description": "Hamster"},
+    {"name": "Sheep", "image": "animal_images/sheep.jpg", "description": "Sheep"},
+    {"name": "Horse", "image": "animal_images/horse.jpg", "description": "Horse"},
+]
+
+mood_explanations = {
+    "Happy": {
+        "description": "Ears are relaxed, a soft gaze, and a wagging tail are the ways of their way of smiling",
+        "fact": "Dogs wag their tails more to the right when they are happy 🐶",
+    },
+    "Sad": {
+        "description": "Decreased activity, avoiding interaction and hiding.",
+        "fact": "Elephants mourn and visit the bones of their dead members in the herd 🐘",
+    },
+    "Angry": {
+        "description": "Showing teeth, growling, focused eyes. Avoid touch and sudden movements",
+        "fact": "Tail movement is a clear indication to back off 🐍",
+    },
+}
