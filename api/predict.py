135ed20a44040d7c44ff638abe085d3207b48c1c
Berkortb <korayybora@gmail.com>
2024-03-27 21:40:56 +0100

    added second opinion message

--- api/predict.py
+++ api/predict.py
@@ -10,9 +10,14 @@ def predict_emotion(image_bytes):
     response_json = response.json()
     print(response.status_code)
     print(response.json())
-    mood = response_json.get("prediction").capitalize()
+    primary_mood = response_json["prediction"][0].capitalize()
+    #mood = response_json.get("prediction").capitalize()
+    try:
+        secondary_mood = response_json["prediction"][1]
+    except IndexError:
+        secondary_mood = None
     classification = response_json.get("classification")
-    description = mood_explanations.get(mood, {}).get("description")
-    fact = mood_explanations.get(mood, {}).get("fact")
+    description = mood_explanations.get(primary_mood, {}).get("description")
+    fact = mood_explanations.get(primary_mood, {}).get("fact")
 
-    return mood, classification, description, fact,
+    return primary_mood, secondary_mood, classification, description, fact
